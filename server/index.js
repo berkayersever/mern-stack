f3470c27f73a63c6bac58eb1c02dd068b12a30fe
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-04 23:54:50 +0300

    Refactored the code.

--- server/index.js
+++ server/index.js
@@ -8,9 +8,17 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/time', (req, res) => res.send({ datetime: new Date().toJSON() }));
-app.get('/v1/users', (req, res) => { res.send(users) });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/time', (req, res) => {
+    res.send({ datetime: new Date().toJSON() });
+});
+
+app.get('/v1/users', (req, res) => {
+    res.send(users);
+});
 
 app.post('/v1/users', (req, res) => {
     const username = req.body.username;
