dd54ac56711c64da80129b62117c34b84feb1cd1
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-04 23:26:13 +0300

    Minor changes.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-import UserModel from './models/User';
-
 const express = require('express');
+const UserModel = require('./models/User');
 const app = express();
 const port = 3000;
 
-const user = new UserModel({id: 'first', email: 'berkay@live.se', username: 'Berkay', role: 'admin'});
-user.getUsername();
+const user1 = new UserModel({id: 'first', email: 'berkay@live.se', username: 'Berkay', role: 'admin'});
+const user2 = new UserModel({id: 'third', email: 'berkay@live.de', username: 'B3rk4y', role: 'customer'});
+const users = [user1.getData(), user2.getData()];
+console.log(users);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/ping', (req, res) => res.send('pong'));
