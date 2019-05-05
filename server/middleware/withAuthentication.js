af6555cb06a4e5c4445514d27090aa5321dc2eb2
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-05 23:08:53 +0300

    Implemented the Authentication Middleware.

--- server/middleware/withAuthentication.js
+++ server/middleware/withAuthentication.js
@@ -1,5 +1,8 @@
 const withAuthentication = (req, res, next) => {    // Authentication Middleware
     // TODO: Is the user authenticated? What's their role?
+    // Dummy for now:
+    req.isAdmin = true;
+    req.isAuthenticated = true;
     next();
 };
 
