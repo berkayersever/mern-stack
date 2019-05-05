b514e0c4d6b9572b9353fad2277c813a879528c6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-05 23:45:56 +0300

    Minor change.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/ping', (req, res) => res.send('pong'));
 app.get('/time', (req, res) => res.send({ datetime: new Date().toJSON() }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
