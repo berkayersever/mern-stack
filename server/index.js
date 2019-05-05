dcd200d77dbdb6536df05a1e5fcf75d18ed2ef4d
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-05 23:50:21 +0300

    Created a new route.

--- server/index.js
+++ server/index.js
@@ -6,5 +6,6 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/time', (req, res) => res.send({ datetime: new Date().toJSON() }));
+app.get('/v1/users', (req, res) => { res.send(users) });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
