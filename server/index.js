567e218b0d88ad5516aa853410e6d50cdac23338
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-05 20:58:34 +0300

    Created a new route for the put method.

--- server/index.js
+++ server/index.js
@@ -28,4 +28,13 @@ app.post('/v1/users', (req, res) => {
     res.status(200).end();
 });
 
+app.put('/v1/users/:id', (req, res) => {
+    const id = req.params.id;
+    const username = req.body.username;
+    const email = req.body.email;
+    const role = req.body.role;
+    console.log('put: data =>', id, username, email, role);
+    res.status(200).end();
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
