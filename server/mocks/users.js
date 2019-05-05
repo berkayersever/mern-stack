f5e854c31e2ecd7addc31fd86df124a1e0b66eb9
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-05 23:43:15 +0300

    Implemented the users.js

--- server/mocks/users.js
+++ server/mocks/users.js
@@ -1,1 +1,7 @@
 import UserModel from '../models/User';
+
+const user1 = new UserModel({ id: 'first', email: 'berkay@live.se', username: 'Berkay', role: 'admin' });
+const user2 = new UserModel({ id: 'third', email: 'berkay@live.de', username: 'B3rk4y', role: 'customer' });
+const users = [user1.getData(), user2.getData()];
+
+export default users;
