b3adfab2e79c81ad49c5023baddf28916163d7af
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-05 23:43:51 +0300

    Refactored the index.js

--- server/index.js
+++ server/index.js
@@ -6,15 +6,6 @@ import UserModel from './models/User';
 const app = express();
 const port = 3000;
 
-const user1 = new UserModel({
-  id: 'first', email: 'berkay@live.se', username: 'Berkay', role: 'admin',
-});
-const user2 = new UserModel({
-  id: 'third', email: 'berkay@live.de', username: 'B3rk4y', role: 'customer',
-});
-const users = [user1.getData(), user2.getData()];
-console.log(users);
-
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/ping', (req, res) => res.send('pong'));
 app.get('/time', (req, res) => res.send({ datetime: new Date().toJSON() }));
