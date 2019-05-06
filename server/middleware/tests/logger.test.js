f2c4bca55219bafe1a93476cd2cca7a481a6028c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-06 04:55:57 +0300

    Minor changes.

--- server/middleware/tests/logger.test.js
+++ server/middleware/tests/logger.test.js
@@ -3,6 +3,7 @@ import logger from '../logger';
 test('It calls next()', () => {
     const next = jest.fn(x => x);
     logger({}, {}, next);
+    logger({}, {}, next);
     console.log(next.mock.calls);
-    expect(next.mock.calls.length).toBe(1);
+    expect(next.mock.calls.length).toBe(2);
 });
