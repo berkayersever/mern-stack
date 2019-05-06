635bf67002f51124c78af1e59a67de0fb3c1f192
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-06 15:00:54 +0300

    Minor changes.

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,10 +1,3 @@
-// Enables the .env file, therefore add a env property to process object.
-// Recommend to require it at the top of the file
-// require('dotenv').config({path: '../.env'});
-import dotenv from 'dotenv';
-dotenv.config({path: '../../.env'});
-console.log(path);
-
 import mongoose from 'mongoose';
 
 console.log(process.env.CONNECTION_STRING);
