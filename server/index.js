19bbc02fd9aa9863bff0094d062551d948b0cb6c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 13:11:01 +0300

    Created endpoints for the products.

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import users from './mocks/users';
 import logger from './middleware/logger';
 import withAuthentication from './middleware/withAuthentication';
 import { UserModel } from './models/User';
+import { ProductModel } from './models/Product';
 import db from './db';
 
 const app = express();
@@ -33,6 +34,11 @@ app.get('/time', (req, res) =>
     res.send({ datetime: new Date().toJSON()
 }));
 
+app.get('/v1/users', async (req, res) => {
+    const users = await UserModel.find() || [];
+    res.send(users);
+});
+
 app.get('/v1/users/:id', async (req, res) => {
     try {
         const user = await UserModel.findById(req.params.id);
@@ -46,11 +52,6 @@ app.get('/v1/users/:id', async (req, res) => {
     catch (e) {
         res.status(404).end;
     }
-});
-
-app.get('/v1/users', async (req, res) => {
-    const users = await UserModel.find() || [];
-    res.send(users);
 });
 
 app.post('/v1/users', (req, res) => {
@@ -68,6 +69,26 @@ app.put('/v1/users/:id', (req, res) => {
     const role = req.body.role;
     console.log('put: data =>', id, username, email, role);
     res.status(200).end();
+});
+
+app.get('/v1/products', async (req, res) => {
+    const products = await ProductModel.find() || [];
+    res.send(products);
+});
+
+app.get('/v1/products/:id', async (req, res) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        if (product) {
+            res.send(product);
+        }
+        else {
+            res.status(404).end;
+        }
+    }
+    catch (e) {
+        res.status(404).end;
+    }
 });
 
 app.delete('/v1/users/:id', (req, res) => {
