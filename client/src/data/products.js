b3d3474c4ba5b0a4deafe7535b847ebb8ee41fc0
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 23:46:08 +0300

    Update products.js

---
+++ client/src/data/products.js
@@ -0,0 +1,61 @@
+const images1 = [
+    'https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png',
+    'https://cdn.bulbagarden.net/upload/1/17/025Pikachu-Original.png'
+];
+
+const images2 = [
+    'https://cdn.bulbagarden.net/upload/3/39/007Squirtle.png',
+    'https://cdn.bulbagarden.net/upload/3/39/007Squirtle.png'
+];
+
+const images3 = [
+    'https://cdn.bulbagarden.net/upload/3/3e/039Jigglypuff.png',
+    'https://cdn.bulbagarden.net/upload/3/3e/039Jigglypuff.png'
+];
+
+const images4 = [
+    'https://cdn.bulbagarden.net/upload/8/88/026Raichu.png',
+    'https://cdn.bulbagarden.net/upload/8/88/026Raichu.png'
+];
+
+const images5 = [
+    'https://cdn.bulbagarden.net/upload/7/7e/006Charizard.png',
+    'https://cdn.bulbagarden.net/upload/7/7e/006Charizard.png'
+];
+
+const images6 = [
+    'https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png',
+    'https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png'
+];
+
+export default [{
+    id: 1,
+    name: 'Pikachu',
+    price: '$49.99',
+    images: images1,
+}, {
+    id: 2,
+    name: 'Squirtle',
+    price: '$39.99',
+    images: images2,
+}, {
+    id: 3,
+    name: 'Jigglypuff',
+    price: '$29.99',
+    images: images3,
+}, {
+    id: 4,
+    name: 'Raichu',
+    price: '$129.00',
+    images: images4,
+}, {
+    id: 5,
+    name: 'Charizard',
+    price: '$55.39',
+    images: images5,
+}, {
+    id: 6,
+    name: 'Bulbasaur',
+    price: '$15.99',
+    images: images6,
+}];
