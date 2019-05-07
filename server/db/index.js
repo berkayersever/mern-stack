bf32479a7296301a6d25a7a4812c1c900d902170
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 12:18:34 +0300

    Initialized the index.js for db

---
+++ server/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true},
+    (err) => {
+        if (err) {
+            console.log('Database Error----------------', err);
+        }
+        console.log('Connected to database');
+    });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.log(error);
+});
+
+db.once('open', () => {
+    console.log('Database connection is open!');
+});
+
+export default db;
