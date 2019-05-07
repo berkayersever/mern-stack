e980c072012a9eb77e87ae1840d463c31738a7ff
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 00:26:50 +0300

    Minor change.

--- client/src/components/inputs/BaseInput.js
+++ client/src/components/inputs/BaseInput.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class BaseInput extends Component {
     render() {
         return (
-            <input type={this.props.type}/>
+            <input {...this.props} />
         )
     }
 }
