8489a78bc6bdfdbc830e352c1d570391b8ddbf7b
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 02:03:34 +0300

    Update NotFound.js

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import products from '../data/products'
-import ProductList from '../components/ProductList';
 
-export default class Home extends Component {
+export default class NotFound extends Component {
     render() {
         return (
-            <ProductList products={products} />
+            <h1>Not Found.</h1>
         );
     }
 }
