a90952acd87f3651dbf3244917c8b08bab97733f
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 00:18:47 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ProductCard from "./components/ProductCard";
+import ProductList from "./components/ProductList";
 import products from './data/products';
 import './App.css';
 
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-
+                <ProductList products={products} />
             </div>
         );
     }
