e953706fca446c543ebf828b51f5d9168089e1b0
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 23:32:35 +0300

    Minor changes in ProductCard.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import ProductCard from "./components/ProductCard";
 import './App.css';
 
+const images = [
+    'https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png',
+    'https://cdn.bulbagarden.net/upload/1/17/025Pikachu-Original.png'
+];
+
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <ProductCard/>
+                <ProductCard images={images} name="Pikachu" price="$49.99"/>
             </div>
         );
     }
--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import './ProductCard.css';
 
-const images = [
-    'https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png',
-    'https://cdn.bulbagarden.net/upload/1/17/025Pikachu-Original.png'
-];
-
 export default class ProductCard extends Component {
-    state = { image: images[0] };
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: this.props.images[0]
+        };
+    }
+    state = { image: this.props.images[0] };
 
     handleMouseOver = () => {
-        this.setState({ image: images[1] });
+        this.setState({ image: this.props.images[1] });
     };
 
     handleMouseLeave = () => {
-        this.setState({ image: images[0] });
+        this.setState({ image: this.props.images[0] });
     };
 
     render() {
         return (
             <div className="ProductCard">
                 <img src={this.state.image} onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave} alt={"Pikachu"}/>
-                <h3>Product Name</h3>
-                <p>$49.99</p>
+                <h3>{this.props.name}</h3>
+                <p>{this.props.price}</p>
             </div>
         )
     }
