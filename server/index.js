9a0e1a18de754cdabac7593f8a8d946d3224ce90
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 15:04:01 +0300

    Refactored the code.

--- server/index.js
+++ server/index.js
@@ -9,27 +9,12 @@ import db from './db';
 
 const app = express();
 const port = 3000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(withAuthentication);
 app.use(logger);
 getUserRoutes(app);
 getProductRoutes(app);
-
-// req => bodyParser.json => bodyParser.urlencoded => withAuthentication => logger => request handler
-
-app.get('/', function (req, res) {
-    throw new Error('BROKEN') // Express will catch this on its own.
-});
-
-app.get('/heartbeat', (req, res) => res.send({
-    dateTime: new Date().toJSON()
-}));
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
 
 app.get('/time', (req, res) =>
     res.send({ datetime: new Date().toJSON()
