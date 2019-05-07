fa380871174cdf760c75acfc8a4a6ee159c4b159
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 23:57:13 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,12 @@ import ProductCard from "./components/ProductCard";
 import products from './data/products';
 import './App.css';
 
-const images = [
-    'https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png',
-    'https://cdn.bulbagarden.net/upload/1/17/025Pikachu-Original.png'
-];
-
 class App extends Component {
     render() {
         return (
             <div className="App">
-                {products.map(product =>
-                    <ProductCard {...product}/>
+                {products.map((product, index) =>
+                    <ProductCard key={index} {...product}/>
                 )}
             </div>
         );
