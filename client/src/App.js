5a1e31b763fe59c6530805501612a0bfc9aac6f8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 02:17:59 +0300

    Major changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import FormDemo from './pages/FormDemo';
 import NotFound from './pages/NotFound';
+import Category from './pages/Category';
+import Orders from './pages/Orders';
+import Cart from './pages/Cart';
+import Account from './pages/Account';
 import './App.css';
-
 
 class App extends Component {
     render() {
@@ -15,7 +18,11 @@ class App extends Component {
                     <Switch>
                         <Route path="/" exact component={Home}/>
                         <Route path="/forms" exact component={FormDemo}/>
-                        <Route component={NotFound} />
+                        <Route path="/cart" exact component={Cart}/>
+                        <Route path="/orders" exact component={Orders}/>
+                        <Route path="/account" exact component={Account}/>
+                        <Route path="/category/:slug" component={Category}/>
+                        <Route component={NotFound}/>
                     </Switch>
                 </Router>
             </div>
