a027b5fa1a0e01eeb36798707bb34328846052f6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 00:21:13 +0300

    Commented out ProductList.

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <ProductList products={products} />
+                {/*<ProductList products={products} />*/}
             </div>
         );
     }
