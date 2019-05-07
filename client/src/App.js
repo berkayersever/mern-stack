3f521feaf74debbd5ef57b1f1d9706968a7d4323
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 00:02:36 +0300

    Minor change.

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                {products.map((product, index) =>
-                    <ProductCard key={index} {...product}/>
+                {products.map((product) =>
+                    <ProductCard key={product.id} {...product}/>
                 )}
             </div>
         );
