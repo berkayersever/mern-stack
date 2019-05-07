a8394b83894b4410e5eae41cc5425f7e9d8534d0
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 12:16:59 +0300

    Minor changes.

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,12 +1,7 @@
-import mongoose, { Schema } from 'mongoose';
-
-const UserSchema = new Schema({
-    email: String,
-    username: String,
-    role: String
-});
-
-const UserModel = mongoose.model('User', UserSchema);
+import mongoose from 'mongoose';
+import { users, products } from './data';
+import { UserModel} from '../models/User';
+import { ProductModel } from '../models/Product';
 
 mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true},
     (err) => {
@@ -24,15 +19,14 @@ db.on('error', (error) => {
 
 db.once('open', () => {
     console.log('Database connection is open!');
-    const user = new UserModel({
-        email: 'berkay@live.se',
-        username: 'Berkay',
-        role: 'admin'
+    UserModel.insertMany(users, (error) => {
+        if (error) {
+            console.log(error);
+        }
     });
-    user.save((error) => {
-        if (error)
-            console.error(error);
-        else
-            console.log('User saved!');
-    })
+    ProductModel.insertMany(products, (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
 });
