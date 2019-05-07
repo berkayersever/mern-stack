ec3f216f824c77c97509b57529bafb74f8daa7de
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 23:27:13 +0300

    Minor changes in ProductCard.

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import './ProductCard.css';
+
+const images = [
+    'https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png',
+    'https://cdn.bulbagarden.net/upload/1/17/025Pikachu-Original.png'
+];
 
 export default class ProductCard extends Component {
+    state = { image: images[0] };
+
+    handleMouseOver = () => {
+        this.setState({ image: images[1] });
+    };
+
+    handleMouseLeave = () => {
+        this.setState({ image: images[0] });
+    };
+
     render() {
         return (
-            <div>
-                <img src={"https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png"}/>
+            <div className="ProductCard">
+                <img src={this.state.image} onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave} alt={"Pikachu"}/>
                 <h3>Product Name</h3>
                 <p>$49.99</p>
             </div>
