18dad8a3c3d625a62632e9c1ebbff163658a60b3
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-07 22:30:58 +0300

    Imported ProductCard to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ProductCard from "./components/ProductCard";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <ProductCard/>
             </div>
         );
     }
