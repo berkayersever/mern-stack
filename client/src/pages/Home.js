c64bf9cda42fa0718bb340846ed2f8785fad7902
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-08 11:16:06 +0300

    Minor fix.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,18 +4,16 @@ import { getProducts } from '../api/Products';
 import LoadingIndicator from '../components/LoadingIndicator';
 
 export default class Home extends Component {
-    state = { products: [], loading: true };
+    state = {products: [], loading: true};
 
     componentDidMount = async () => {
-        const products = await getProducts();
-        this.setState({ products, loading: false });
+        const products = await getProducts() || [];
+        this.setState({products, loading: false});
     };
 
     render() {
         return (
-            this.state.loading ?
-                <LoadingIndicator/> :
-            <ProductList products={this.state.products} />
+            this.state.loading ? <LoadingIndicator/> :<ProductList products={this.state.products}/>
         );
     }
 }
