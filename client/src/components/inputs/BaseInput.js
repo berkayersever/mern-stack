c580f57dfce472edc24f613476f340df11790acb
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-09 17:27:04 +0300

    Minor changes in BaseInput.js

--- client/src/components/inputs/BaseInput.js
+++ client/src/components/inputs/BaseInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from'prop-types';
 import './BaseInput.css';
 
 export default class BaseInput extends Component {
@@ -6,10 +7,16 @@ export default class BaseInput extends Component {
         return (
             <div className={ `BaseInput ${this.props.type === 'checkbox' ? 'BaseInputReverse' : ''}` }>
                 <label htmlFor={this.props.name}>
-                    {this.props.label || ''}
+                    {this.props.label}
                 </label>
                 <input id={this.props.name} {...this.props} />
             </div>
         )
     }
 }
+
+BaseInput.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['checkbox', 'text', 'password']).isRequired
+};
