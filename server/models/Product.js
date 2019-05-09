72dde5353060abfbb84c0b2e3745868194d51760
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-09 22:10:39 +0300

    Minor changes in Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,55 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
 
-export default class Product {
-    /**
-     * @param {string} id
-     * @param {string} name
-     * @param {number} price
-     * @param {Array.<string>} images
-     */
-    constructor({id, name, price, images}) {
-        this._id = id;
-        this._name = name;
-        this._price = price;
-        this._images = images;
-    }
-
-    /**
-     * @returns {string}
-     */
-    getId = () => this._id;
-
-    /**
-     * @returns {string}
-     */
-    getName = () => this._name;
-
-    /**
-     * @returns {number}
-     */
-    getPrice = () => this._price;
-
-    /**
-     * @returns {Array<string>}
-     */
-    getImages = () => this._images;
-
-    /**
-     * @returns {{id: (string), name: (string), price: (number), images: (Array<string>)}}
-     */
-    getData = () => ({
-        id: this._id,
-        name: this._name,
-        price: this._price,
-        images: this._images
-    });
-}
-
 export const ProductSchema = new Schema({
     name: String,
     price: Number,
     images: [String],
-    categories: [String]
+    categories: [String],
+    featured: Boolean
 });
 
 export const ProductModel = mongoose.model('Product', ProductSchema);
