64a9b47faac43ea7000879798382d164f1f135c8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-12 22:36:27 +0300

    Minor changes.

--- server/models/Pokemon.js
+++ server/models/Pokemon.js
@@ -1,7 +1,12 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const PokemonSchema = new Schema({
-
+    id: Number,
+    name: String,
+    price: Number,
+    images: [String],
+    categories: [String],
+    featured: Boolean
 });
 
 export const PokemonModel = mongoose.model('Pokemon', ProductSchema);
