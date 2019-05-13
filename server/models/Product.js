c810541ca6db64b4f7aaf99f75858de4472333b5
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-13 21:32:10 +0300

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,11 +1,21 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const ProductSchema = new Schema({
+    pokedex: Number,
     name: String,
     price: Number,
     images: [String],
     categories: [String],
-    featured: Boolean
+    featured: Boolean,
+    total: Number,
+    hp: Number,
+    attack: Number,
+    defense: Number,
+    sp_atk: Number,
+    sp_def: Number,
+    speed: Number,
+    generation: Number,
+    legendary: Boolean
 });
 
 export const ProductModel = mongoose.model('Product', ProductSchema);
