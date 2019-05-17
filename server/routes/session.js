fccf2375f126b16f7668fe77ac2e28d773263b40
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-18 02:20:48 +0300

    Implemented 3 routes for the session.

--- server/routes/session.js
+++ server/routes/session.js
@@ -3,3 +3,47 @@ import Joi from 'joi';
 import { UserModel } from '../models/User';
 import { signin } from '../validations/user';
 import { parseError, sessionizeUser } from '../scripts/helpers';
+// const Joi = require('joi');
+// const sessionRouter = express.Router();
+
+export default (app) => {
+    app.post('', async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            await Joi.validate({ email, password }, signin);
+
+            const user = await UserModel.findOne({ email });
+            if (user && user.comparePasswords(password)) {
+                const sessionUser = sessionizeUser(user);
+
+                req.session.user = sessionUser;
+                res.send(sessionUser);
+            } else {
+                throw new Error('Invalid login credentials');
+            }
+        } catch (err) {
+            res.status(401).send(parseError(err));
+        }
+    });
+
+    app.delete('', ({ session }, res) => {
+        try {
+            const user = session.user;
+            if (user) {
+                session.destroy(err => {
+                    if (err) throw (err);
+                    res.clearCookie(process.env.SESSION_NAME);
+                    res.send(user);
+                });
+            } else {
+                throw new Error('Something went wrong');
+            }
+        } catch (err) {
+            res.status(422).send(parseError(err));
+        }
+    });
+
+    app.get('', ({ session: { user }}, res) => {
+        res.send({ user });
+    });
+};
