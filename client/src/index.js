5c431b40f06d98d432dd40f3fb1b254733b3b2c0
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-18 02:48:10 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let preloadedState = {};
+const store = configureStore(preloadedState);
+
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
+
+// TODO: For Testing! Remove before production.
+window.getState = store.getState;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
