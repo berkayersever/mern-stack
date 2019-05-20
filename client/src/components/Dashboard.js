9fbd0c3e48496738fb84f936926f1b1d453be3b4
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-20 23:27:08 +0300

    Minor change.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import { login } from '../actions/session';
+import { logout } from '../actions/session';
 
 const mapStateToProps = ({ session }) => ({
     session
