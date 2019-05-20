51afc11a4e83eae68d26b248c0fcadfa026ba4c0
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-20 23:22:47 +0300

    Minor changes.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
-import React, {Fragment} from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {login} from '../actions/session';
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login } from '../actions/session';
 
 const mapStateToProps = ({errors}) => ({
     errors
