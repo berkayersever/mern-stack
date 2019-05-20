07038bd778556466a5f9ba6173302c2ba8a0c3a9
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-20 23:26:22 +0300

    Implemented the Dashboard component.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,3 +1,21 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions/session';
+
+const mapStateToProps = ({ session }) => ({
+    session
+});
+
+const mapDispatchToProps = dispatch => ({
+    logout: () => dispatch(logout())
+});
+
+const Dashboard = ({ logout, session }) => (
+    <Fragment>
+        <h1>Hi {session.username}!</h1>
+        <p>You are logged in</p>
+        <button onClick={logout}>Logout</button>
+    </Fragment>
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
